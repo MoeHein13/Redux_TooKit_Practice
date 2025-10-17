fae4ea57d81eb39330f12f7aa82595c5b7d14508
Kelvin <kelvinmoe96@gmail.com>
2025-10-17 22:48:06 +0800

    add from

--- src/Components/CarForm.tsx
+++ src/Components/CarForm.tsx
@@ -1,5 +1,16 @@
 const CarForm = () => {
-  return <div>CarForm</div>;
+  return (
+    <>
+      <div className="flex items-center">
+        <h1 className="text-xl font-semibold">CarForm</h1>
+
+        <form>
+          <label htmlFor="name">Name</label>
+          <input type="text" />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default CarForm;
